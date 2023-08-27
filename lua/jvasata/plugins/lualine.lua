c69d57d78ebcde48b632301d9816ae57d78035d4
Josef Vasata <vasatjos@fit.cvut.cz>
2023-08-27 12:23:52 +0200

    Edit lualine colorscheme

--- lua/jvasata/plugins/lualine.lua
+++ lua/jvasata/plugins/lualine.lua
@@ -1,7 +1,7 @@
 local custom_onedark = require ("lualine.themes.onedark")
 
-custom_onedark.normal.a.bg = "#98C379"
-custom_onedark.insert.a.bg = "#61AFEF"
+custom_onedark.normal.a.bg = "#61AFEF"
+custom_onedark.insert.a.bg = "#98C379"
 custom_onedark.visual.a.bg = "#C678DD"
 custom_onedark.command = {
     a = {
