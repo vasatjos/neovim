3ede6ec14f8ce38a6b7feec73fb6a92cb012319b
Josef Vasata <vasatjos@fit.cvut.cz>
2023-09-20 16:28:24 +0200

    Add live grep keymap

--- lua/jvasata/plugins/telescope.lua
+++ lua/jvasata/plugins/telescope.lua
@@ -3,3 +3,4 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {}) -- find file
 vim.keymap.set("n", "<leader>fw", function()              -- find word
         builtin.grep_string({ search = vim.fn.input ("Find word: ") })
     end)
+vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
