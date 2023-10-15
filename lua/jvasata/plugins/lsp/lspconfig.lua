2b8850d4da06dca4f6be7b4b99cc0e035ce016fc
Josef Vasata <vasatjos@fit.cvut.cz>
2023-10-15 11:49:20 +0200

    Remove lsp keymap clash with ReplaceWithRegister

--- lua/jvasata/plugins/lsp/lspconfig.lua
+++ lua/jvasata/plugins/lsp/lspconfig.lua
@@ -5,7 +5,7 @@ lsp.preset("recommended")
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 
-  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = true})
+  vim.keymap.set('n', '<leader>gr', '<cmd>Telescope lsp_references<cr>', {buffer = true})
 end)
 
 lsp.setup()
