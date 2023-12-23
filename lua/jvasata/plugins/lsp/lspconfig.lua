52cd01daf01e921430fc1e1d52228f60778866b8
Josef Vasata <vasatjos@fit.cvut.cz>
2023-12-23 13:19:57 +0100

    Add auto-format keybinding

--- lua/jvasata/plugins/lsp/lspconfig.lua
+++ lua/jvasata/plugins/lsp/lspconfig.lua
@@ -1,12 +1,13 @@
-local lsp = require ("lsp-zero")
+local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+    lsp.default_keymaps({ buffer = bufnr })
 
-  vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", {buffer = true})
-  vim.keymap.set("n", "<leader>ca",function () vim.lsp.buf.code_action() end, {buffer = true})
+    vim.keymap.set("n", "<leader>gr", "<cmd>Telescope lsp_references<cr>", { buffer = true })
+    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, { buffer = true })
+    vim.keymap.set("n", "<leader>afm", "<cmd>LspZeroFormat<CR>", { buffer = true }) -- [A]uto [f]or[m]at
 end)
 
 lsp.setup()
