b1cb8fc6cb0088b312c6c523c692f8689660c7e5
Josef Vasata <vasatjos@fit.cvut.cz>
2023-12-28 18:27:01 +0100

    Treesitter config update

--- lua/jvasata/plugins/treesitter.lua
+++ lua/jvasata/plugins/treesitter.lua
@@ -1,17 +1,18 @@
 -- import nvim-treesitter plugin safely
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-  return
+    return
 end
 
 -- configure treesitter
 treesitter.setup({
-  -- enable syntax highlighting
-  highlight = {
-    enable = true,
-  },
-  -- enable indentation
-  indent = { enable = true },
-  -- enable autotagging (w/ nvim-ts-autotag plugin)
-  autotag = { enable = true },
+    -- enable syntax highlighting
+    highlight = {
+        enable = true,
+    },
+    -- enable indentation
+    indent = { enable = true },
+    -- enable autotagging (w/ nvim-ts-autotag plugin)
+    autotag = { enable = true },
+    auto_install = true,
 })
