3af2ab783f7d483829ec823e12d7d2f97c91aeec
Josef Vasata <vasatjos@fit.cvut.cz>
2024-01-16 16:47:36 +0100

    The basics

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,39 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>rw", vim.cmd.Ex) -- open Netrw
+
+keymap.set("n", "x", '"_x')                 -- don't copy deleted characters
+
+keymap.set("n", "<leader>sv", "<C-w>v")     -- vertical split
+keymap.set("n", "<leader>sh", "<C-w>s")     -- horizontal split
+keymap.set("n", "<leader>se", "<C-w>=")     -- set width equal
+keymap.set("n", "<leader>sx", ":close<CR>") -- close
+
+keymap.set("n", "<leader>ch", ":nohl<CR>")  -- clear highlights
+
+keymap.set("n", "<leader><leader>+", "<C-a>")
+keymap.set("n", "<leader><leader>-", "<C-x>")
+
+keymap.set("i", "<C-h>", "<Left>")
+keymap.set("i", "<C-j>", "<Down>")
+keymap.set("i", "<C-k>", "<Up>")
+keymap.set("i", "<C-l>", "<Right>")
+keymap.set("i", "<C-w>", "<C-o>w")
+keymap.set("i", "<C-b>", "<C-o>b")
+
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+keymap.set("n", "J", "mzJ`z")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
+keymap.set("x", "<leader>p", [["_dP]])       -- paste over without replacing clipboard
+keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- system clipboard
+keymap.set("n", "<leader>Y", [["+Y]])        -- system clipboard
+keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+keymap.set("n", "Q", "<nop>")
+keymap.set("n", "<leader>fd", vim.lsp.buf.format)
---
+++ lua/core/options.lua
@@ -0,0 +1,44 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+
+-- line wrap
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+opt.hlsearch = false
+
+opt.cursorline = true
+
+opt.swapfile = false
+opt.updatetime = 50
+
+opt.scrolloff = 8
+opt.sidescrolloff = 10
+
+--appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+opt.colorcolumn = "80"
+
+opt.backspace = "indent,eol,start"
+
+-- use system clipboard
+--opt.clipboard = "unnamedplus"
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
