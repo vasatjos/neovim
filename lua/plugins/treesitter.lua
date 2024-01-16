724ff5f0086767ac1c8e076cdd9e019c724ebe41
Josef Vasata <vasatjos@fit.cvut.cz>
2024-01-16 16:48:50 +0100

    Add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return     {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "javascript", "html" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+            autotag = { enable = true}, 
+            auto_install = true
+        })
+    end
+}
