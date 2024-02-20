9fba520f0fcdd8f7f4e0682eb72b39871ac35b36
Josef Vasata <vasatjos@fit.cvut.cz>
2024-02-20 15:52:30 +0100

    Add command line completions

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,6 +4,7 @@ return {
         dependencies = {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
             "L3MON4D3/LuaSnip",             -- snippet engine
             "saadparwaiz1/cmp_luasnip",     -- for autocompletion
             "rafamadriz/friendly-snippets", -- useful snippets
@@ -11,7 +12,7 @@ return {
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
-            require ("luasnip/loaders/from_vscode").lazy_load()
+            require("luasnip/loaders/from_vscode").lazy_load()
             vim.opt.completeopt = "menu,menuone,noselect"
             cmp.setup({
                 snippet = {
@@ -25,7 +26,7 @@ return {
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-                    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+                    ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
                     ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                     ["<CR>"] = cmp.mapping.confirm({ select = false }),
                 }),
@@ -34,9 +35,28 @@ return {
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
                     { name = "luasnip" }, -- snippets
-                    { name = "buffer" }, -- text within current buffer
-                    { name = "path" }, -- file system paths
+                    { name = "buffer" },  -- text within current buffer
+                    { name = "path" },    -- file system paths
                 }),
+            })
+            cmp.setup.cmdline('/', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
+                })
             })
         end
     }
