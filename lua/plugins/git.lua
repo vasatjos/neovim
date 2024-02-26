763b3a4f16a946006219d2517ab50b83e1f3ef41
Josef Vasata <vasatjos@fit.cvut.cz>
2024-02-26 20:06:41 +0100

    Add fugitive
    TODO: Fix null-ls errors when running a diff split

---
+++ lua/plugins/git.lua
@@ -0,0 +1,8 @@
+return {
+    "tpope/vim-fugitive",
+    config = function()
+        vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit!<CR>", {}) -- TODO: Fix null-ls errors
+        vim.keymap.set("n", "<leader>gh", "<cmd>diffget //2<CR>")
+        vim.keymap.set("n", "<leader>gl", "<cmd>diffget //3<CR>")
+    end,
+}
