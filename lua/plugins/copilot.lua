e67f41ee0cbca76412bb0064064d4307b7b14ed1
Josef Vasata <vasatjos@fit.cvut.cz>
2024-04-03 11:54:11 +0200

    More copilot keymaps

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -6,11 +6,15 @@ return {
                 expr = true,
                 replace_keycodes = false
             })
-            vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+            vim.keymap.set('i', '<M-y>', 'copilot#Accept("\\<CR>")', {
                 expr = true,
                 replace_keycodes = false
             })
             vim.g.copilot_no_tab_map = true
+            vim.keymap.set('i', '<M-w>', '<Plug>(copilot-accept-word)')
+            vim.keymap.set('i', '<M-l>', '<Plug>(copilot-accept-line)')
+            vim.keymap.set('i', '<M-Right>', '<Plug>(copilot-next)')
+            vim.keymap.set('i', '<M-Left>', '<Plug>(copilot-prev)')
         end
     }
 }
