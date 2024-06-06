50fd504d88a5da985386f11e1ef2e166424b05f3
Josef Vasata <vasatjos@fit.cvut.cz>
2024-06-06 10:56:05 +0200

    Kanagawa theme

--- lua/plugins/basics.lua
+++ lua/plugins/basics.lua
@@ -1,18 +1,34 @@
 return {
     {
-        "olimorris/onedarkpro.nvim",
-        priority = 1000, -- Ensure it loads first
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
         config = function()
-            local onedark = require("onedarkpro")
-            onedark.setup({
-                options = {
-                    cursorline = true
+            require("kanagawa").setup({
+                keywordStyle = { italic = false },
+                -- Remove sign column background
+                colors = {
+                    theme = {
+                        all = {
+                            ui = { bg_gutter = "none" }
+                        }
+                    }
                 },
+                overrides = function(colors)
+                    local theme = colors.theme
+                    return {
+                        -- make completions menu background darker
+                        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+                        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+                        PmenuSbar = { bg = theme.ui.bg_m1 },
+                        PmenuThumb = { bg = theme.ui.bg_p2 },
+                    }
+                end,
             })
-            vim.cmd("colorscheme onedark")
-        end,
+            vim.cmd("colorscheme kanagawa-wave")
+        end
     },
-    "christoomey/vim-tmux-navigator", -- Ctrl + motions to change split focus
+    "christoomey/vim-tmux-navigator",
     "tpope/vim-surround",
     {
         "numToStr/Comment.nvim",
