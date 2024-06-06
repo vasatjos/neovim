2ab6a0492d8745a9ac122de3aea93cc06ec2f3e1
Josef Vasata <vasatjos@fit.cvut.cz>
2024-06-06 10:56:05 +0200

    Kanagawa theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,3 @@
-local custom_onedark = require ("lualine.themes.onedark")
-
-custom_onedark.normal.a.bg = "#61AFEF"
-custom_onedark.insert.a.bg = "#98C379"
-custom_onedark.visual.a.bg = "#C678DD"
-custom_onedark.command = {
-    a = {
-        gui = "bold",
-        bg = "#E5C07B",
-        fg = "#282C34"
-    }
-}
-
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -19,8 +6,12 @@ return {
     config = function()
         require("lualine").setup({
             options = {
-                theme = custom_onedark
-            }
+                theme = 'auto',
+                globalstatus = true,
+            },
+            sections = {
+                lualine_x = { 'filetype' },     -- remove encoding and fileformat
+            },
         })
     end,
 
