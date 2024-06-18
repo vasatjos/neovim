5364575938b74f0ecb543d28300607daa1a85991
Josef Vasata <vasatjos@fit.cvut.cz>
2024-06-18 12:22:22 +0200

    Add quickfix list keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,38 +1,41 @@
 vim.g.mapleader = " "
 
-local keymap = vim.keymap
+local map = vim.keymap.set
 
 -- keymap.set("n", "<leader>rw", vim.cmd.Ex) -- open Netrw, uncomment if not using a different file browser
 
-keymap.set("n", "x", '"_x')                 -- don't copy deleted characters
+map("n", "x", '"_x')                 -- don't copy deleted characters
 
-keymap.set("n", "<leader>sv", "<C-w>v")     -- vertical split
-keymap.set("n", "<leader>sh", "<C-w>s")     -- horizontal split
-keymap.set("n", "<leader>se", "<C-w>=")     -- set width equal
-keymap.set("n", "<leader>sx", ":close<CR>") -- close
+map("n", "<leader>sv", "<C-w>v")     -- vertical split
+map("n", "<leader>sh", "<C-w>s")     -- horizontal split
+map("n", "<leader>se", "<C-w>=")     -- set width equal
+map("n", "<leader>sx", ":close<CR>") -- close
 
-keymap.set("n", "<Esc>", ":nohl<CR>")  -- clear highlights
+map("n", "<Esc>", ":nohl<CR>")  -- clear highlights
 
-keymap.set("i", "<C-h>", "<Left>")
-keymap.set("i", "<C-j>", "<Down>")
-keymap.set("i", "<C-k>", "<Up>")
-keymap.set("i", "<C-l>", "<Right>")
-keymap.set("i", "<C-w>", "<C-o>w")
-keymap.set("i", "<C-b>", "<C-o>b")
+map("i", "<C-h>", "<Left>")
+map("i", "<C-j>", "<Down>")
+map("i", "<C-k>", "<Up>")
+map("i", "<C-l>", "<Right>")
+map("i", "<C-w>", "<C-o>w")
+map("i", "<C-b>", "<C-o>b")
 
-keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
 
-keymap.set("n", "yA", "ggVGy")              -- yank all
-keymap.set("n", "<leader>yA", 'ggVG"+y')    -- yank all to system clipboard
+map("n", "yA", "ggVGy")              -- yank all
+map("n", "<leader>yA", 'ggVG"+y')    -- yank all to system clipboard
 
-keymap.set("n", "J", "mzJ`z")
-keymap.set("n", "<C-d>", "<C-d>zz")
-keymap.set("n", "<C-u>", "<C-u>zz")
-keymap.set("n", "n", "nzzzv")
-keymap.set("n", "N", "Nzzzv")
-keymap.set("x", "<leader>p", [["_dP]])       -- paste over without replacing clipboard
-keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- system clipboard
-keymap.set("n", "<leader>Y", [["+Y]])        -- system clipboard
-keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-keymap.set("n", "Q", "<nop>")
+map("n", "<Up>", vim.cmd.cprev)
+map("n", "<Down>", vim.cmd.cnext)
+
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("x", "<leader>p", [["_dP]])       -- paste over without replacing clipboard
+map({ "n", "v" }, "<leader>y", [["+y]]) -- system clipboard
+map("n", "<leader>Y", [["+Y]])        -- system clipboard
+map({ "n", "v" }, "<leader>d", [["_d]])
+map("n", "Q", "<nop>")
