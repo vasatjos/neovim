fc3dde662e322ab6c50f4714d1c5160b606d6fd7
Josef Vasata <vasatjos@fit.cvut.cz>
2024-07-07 23:17:53 +0200

    improve makefile compatibility

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -61,9 +61,17 @@ end
 -- Highlight when yanking (copying) text
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- Turn off expandtab for Makefiles
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'make',
+    callback = function()
+        vim.opt_local.expandtab = false
+    end,
 })
