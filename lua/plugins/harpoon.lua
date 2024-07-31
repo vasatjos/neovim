1d9a0cf5bd8c2c37b567f45d5c7793ff41b14c6d
Josef Vasata <vasatjos@fit.cvut.cz>
2024-07-31 12:40:18 +0200

    Remove deprecated harpoon settings

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -36,7 +36,7 @@ return {
         vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
             { desc = "Open harpoon window" })
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader>h", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
