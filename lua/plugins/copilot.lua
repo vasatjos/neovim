ee89b573ca6c799cce3cacff57a2694a6f466d5c
Josef Vasata <vasatjos@fit.cvut.cz>
2024-10-10 16:23:01 +0200

    Disable copilot by default

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,10 @@
 return {
     {
         "github/copilot.vim",
+        event = "InsertEnter",
         config = function()
+            vim.g.copilot_no_tab_map = true
+            vim.g.copilot_enabled = false
             vim.keymap.set('i', '<S-Right>', 'copilot#Accept("\\<CR>")', {
                 expr = true,
                 replace_keycodes = false
@@ -10,11 +13,14 @@ return {
                 expr = true,
                 replace_keycodes = false
             })
-            vim.g.copilot_no_tab_map = true
             vim.keymap.set('i', '<M-w>', '<Plug>(copilot-accept-word)')
             vim.keymap.set('i', '<M-l>', '<Plug>(copilot-accept-line)')
             vim.keymap.set('i', '<M-Right>', '<Plug>(copilot-next)')
             vim.keymap.set('i', '<M-Left>', '<Plug>(copilot-prev)')
+            vim.keymap.set('i', '<M-e>', 'copilot#Dismiss()', {
+                expr = true,
+                silent = true
+            })
         end
     }
 }
