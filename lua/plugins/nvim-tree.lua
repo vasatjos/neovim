ab7555dc9204e66b959e4b832e87cb14e20303d4
Josef Vasata <vasatjos@fit.cvut.cz>
2025-01-12 14:50:10 +0100

    Improve nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,13 +7,32 @@ return {
         -- disable netrw
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
-
         require("nvim-tree").setup({
+            renderer = {
+                icons = {
+                    glyphs = {
+                        folder = {
+                            arrow_closed = "", -- arrow when folder is closed
+                            arrow_open = "", -- arrow when folder is open
+                        },
+                    },
+                },
+            },
+            actions = {
+                open_file = {
+                    window_picker = {
+                        enable = false,
+                    },
+                },
+            },
+            git = {
+                ignore = false,
+            },
             view = {
                 width = 45,
                 relativenumber = true
             },
         })
-        vim.keymap.set("n", "<leader>rw", ":NvimTreeToggle<CR>")
-    end,
+        vim.keymap.set("n", "<leader>rw", vim.cmd.NvimTreeToggle)
+    end
 }
