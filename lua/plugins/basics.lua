595ef66d046a25ecd3b143813c2b72d438872a99
Josef Vasata <vasatjos@fit.cvut.cz>
2025-03-19 15:53:44 +0100

    Add indent guides plugin

--- lua/plugins/basics.lua
+++ lua/plugins/basics.lua
@@ -40,4 +40,16 @@ return {
             require("colorizer").setup()
         end,
     },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        config = function()
+            require("ibl").setup({
+                scope = {
+                    -- don't highlight start of indent block nor return
+                    show_start = false,
+                    show_end = false,
+                }
+            })
+        end
+    },
 }
