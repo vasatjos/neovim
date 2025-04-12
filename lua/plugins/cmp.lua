7a01ed04f5d0f70cfc30da73a3d5cd573da14d95
Josef Vasata <vasatjos@fit.cvut.cz>
2025-04-12 12:35:27 +0200

    Add LSP to cmp sources

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,6 +2,7 @@ return {
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
@@ -25,8 +26,7 @@ return {
                     ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),             -- show completion suggestions
-                    ["<C-e>"] = cmp.mapping.abort(),                    -- close completion window
+                    ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
                     ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                     ["<CR>"] = cmp.mapping.confirm({ select = false }),
