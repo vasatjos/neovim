d29fbf60de12963fdf97028ba47beab816005d75
Josef Vasata <vasatjos@fit.cvut.cz>
2025-04-12 16:49:25 +0200

    Stylua formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,12 +15,5 @@ return {
             })
         end
     },
-    {
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({
-				max_lines = 5,
-			})
-		end,
-	}
+    "nvim-treesitter/nvim-treesitter-context",
 }
