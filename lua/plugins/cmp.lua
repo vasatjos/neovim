18c474a3255e9e4534122bbc0b7d34f6b70ec000
Josef Vasata <vasatjos@fit.cvut.cz>
2025-06-22 17:44:33 +0200

    Edit cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -15,6 +15,29 @@ return {
             local luasnip = require("luasnip")
             require("luasnip/loaders/from_vscode").lazy_load()
             vim.opt.completeopt = "menu,menuone,noselect"
+
+            local function prefer_shorter_entries(entry1, entry2)
+                local word1 = entry1.completion_item.label
+                local word2 = entry2.completion_item.label
+                if word1:sub(1, #word2) == word2 and #word1 ~= #word2 then
+                    return #word1 < #word2
+                elseif word2:sub(1, #word1) == word1 and #word1 ~= #word2 then
+                    return #word1 < #word2
+                end
+            end
+
+            local function under_comparator(entry1, entry2)             -- "lukas-reineke/cmp-under-comparator"
+                local _, entry1_under = entry1.completion_item.label:find "^_+"
+                local _, entry2_under = entry2.completion_item.label:find "^_+"
+                entry1_under = entry1_under or 0
+                entry2_under = entry2_under or 0
+                if entry1_under > entry2_under then
+                    return false
+                elseif entry1_under < entry2_under then
+                    return true
+                end
+            end
+
             cmp.setup({
                 snippet = {
                     expand = function(args)
@@ -26,7 +49,7 @@ return {
                     ["<C-n>"] = cmp.mapping.select_next_item(), -- next suggestion
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
+                    ["<C-e>"] = cmp.mapping.abort(), -- close completion window
                     ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                     ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                     ["<CR>"] = cmp.mapping.confirm({ select = false }),
@@ -45,42 +68,33 @@ return {
                         cmp.config.compare.offset,
                         cmp.config.compare.exact,
                         cmp.config.compare.score,
+                        prefer_shorter_entries,
                         cmp.config.compare.recently_used,
-                        function(entry1, entry2) -- "lukas-reineke/cmp-under-comparator"
-                            local _, entry1_under = entry1.completion_item.label:find "^_+"
-                            local _, entry2_under = entry2.completion_item.label:find "^_+"
-                            entry1_under = entry1_under or 0
-                            entry2_under = entry2_under or 0
-                            if entry1_under > entry2_under then
-                                return false
-                            elseif entry1_under < entry2_under then
-                                return true
-                            end
-                        end,
+                        under_comparator,
                         cmp.config.compare.kind,
                     },
                 },
             })
-            cmp.setup.cmdline('/', {
+            cmp.setup.cmdline("/", {
                 mapping = cmp.mapping.preset.cmdline(),
                 sources = {
-                    { name = 'buffer' },
-                    { name = 'path' }
-                }
+                    { name = "buffer" },
+                    { name = "path" },
+                },
             })
-            cmp.setup.cmdline(':', {
+            cmp.setup.cmdline(":", {
                 mapping = cmp.mapping.preset.cmdline(),
                 sources = cmp.config.sources({
-                    { name = 'path' }
+                    { name = "path" },
                 }, {
                     {
-                        name = 'cmdline',
+                        name = "cmdline",
                         option = {
-                            ignore_cmds = { 'Man', }
-                        }
-                    }
-                })
+                            ignore_cmds = { "Man" },
+                        },
+                    },
+                }),
             })
-        end
-    }
+        end,
+    },
 }
