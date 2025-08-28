6397cb29932b1a8b93b63dac2ae4d3b7d4f89c1b
Josef Vasata <jvasata03@gmail.com>
2025-08-28 23:02:53 +0200

    Edit treesitter ensure installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc", "javascript", "html", "css" },
+                ensure_installed = { "c", "cpp", "python", "lua"},
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
